16d04b7683250aafe84ead431550effeadd368d2
vedran <vedran.primorac@gmail.com>
2021-09-12 22:30:36 +0200

    Initial frontend commit. Angular 11 based project. Will update to 12 later. Dockerized. Routing established. Almost unstyled. Light/dark theme. Added a plotly chart with mock data.

---
+++ Frontend/src/app/app.module.ts
@@ -0,0 +1,61 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
+import {MatMenuModule} from '@angular/material/menu';
+import {MatCardModule} from '@angular/material/card';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {MatDialogModule} from '@angular/material/dialog';
+import {A11yModule} from '@angular/cdk/a11y';
+
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { AboutComponent } from './about/about.component';
+import { IntroComponent } from './intro/intro.component';
+
+import {OverlayContainer} from '@angular/cdk/overlay';
+
+import * as PlotlyJS from 'plotly.js-dist-min';
+import { PlotlyModule } from 'angular-plotly.js';
+
+PlotlyModule.plotlyjs = PlotlyJS;
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    AboutComponent,
+    IntroComponent,
+  ],
+  imports: [
+    A11yModule,
+    BrowserModule,
+    MatButtonModule,
+    AppRoutingModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatSlideToggleModule,
+    MatMenuModule,
+    MatDialogModule,
+    PlotlyModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { 
+  constructor(overlayContainer: OverlayContainer) {
+    overlayContainer.getContainerElement().classList.add('dark-theme');
+  }
+}
---
+++ Frontend/src/app/intro/intro.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-intro',
+  templateUrl: './intro.component.html',
+  styleUrls: ['./intro.component.scss']
+})
+export class IntroComponent {
+
+  
+  public graph = {
+    data: [
+      { x: [1, 2, 3], y: [2, 6, 3], type: 'scatter', mode: 'lines+points', marker: { color: 'red' } },
+      { x: [1, 2, 3], y: [2, 5, 3], type: 'bar' },
+    ],
+    layout: {  title: 'A Fancy Plot' }
+  };
+
+  constructor(private router: Router) { }
+
+
+
+}
