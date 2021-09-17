71e53b35f3976deb4c471918d6716ef885bdbdc7
Mister_Eth <89896648+WhoEvenAmI@users.noreply.github.com>
2021-09-17 13:12:47 +0300

    Add Arbitrum provider

---
+++ ETHTPS.API/ETHTPS.TPSLogger/ArbiscanLogger.cs
@@ -0,0 +1,61 @@
+﻿using ETHTPS.API.Infrastructure.Database.Models;
+
+using Newtonsoft.Json;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ETHTPS.TPSLogger
+{
+    public class ArbiscanLogger : TPSLoggerBase
+    {
+        private readonly string _apiKey;
+        public ArbiscanLogger(ETHTPSContext Context, string name, string apiKey) : base(Context, name)
+        {
+            _apiKey = apiKey;
+        }
+
+        public override async void LogDataAsync()
+        {
+            while (true)
+            {
+                var client = new HttpClient();
+                try
+                {
+                    int latestBlock = HexToDec(JsonConvert.DeserializeObject<dynamic>(await client.GetStringAsync($"https://api.arbiscan.io/api?module=proxy&action=eth_blockNumber&apikey={_apiKey}")).result.ToString());
+                    int blockTransactions = HexToDec(JsonConvert.DeserializeObject<dynamic>(await client.GetStringAsync($"https://api.arbiscan.io/api?module=proxy&action=eth_getBlockTransactionCountByNumber&apikey={_apiKey}&tag={latestBlock.ToString("X")}")).result.ToString());
+                    lock (Program.LockObject)
+                    {
+                        var provider = Context.Providers.First(x => x.Name == "Ethereum");
+                        var data = new TPSData()
+                        {
+                            Block = latestBlock.ToString(),
+                            Date = DateTime.Now,
+                            Provider = provider.Id,
+                            Tps = (int)(Math.Ceiling(blockTransactions / 13.1))
+                        };
+                        if (!Context.Tpsdata.Any(x => x.Provider.Value == provider.Id && x.Block == latestBlock.ToString()))
+                        {
+                            Context.Tpsdata.Add(data);
+                        }
+                        Context.SaveChanges();
+                        Console.WriteLine($"{Name} block {latestBlock} transaction count: {blockTransactions} ({data.Tps} tps)");
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+                await Task.Delay(10000);
+            }
+        }
+        private static int HexToDec(string hex)
+        {
+            return int.Parse(hex.Replace("0x", string.Empty), System.Globalization.NumberStyles.HexNumber);
+        }
+    }
+}
--- ETHTPS.API/ETHTPS.TPSLogger/Program.cs
+++ ETHTPS.API/ETHTPS.TPSLogger/Program.cs
@@ -26,71 +26,13 @@ namespace ETHTPS.TPSLogger
             .Build();
             services.AddDbContext<ETHTPSContext>(options => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
             var provider = services.BuildServiceProvider();
-            UpdateMainnetTPS(provider.GetRequiredService<ETHTPSContext>(), configuration.GetSection("APIKeys").GetValue<string>("Etherscan"));
+            var etherscanLogger= new EtherscanLogger(provider.GetRequiredService<ETHTPSContext>(), "Ethereum", configuration.GetSection("APIKeys").GetValue<string>("Etherscan"));
+            etherscanLogger.LogDataAsync();
+            var arbiscanLogger = new ArbiscanLogger(provider.GetRequiredService<ETHTPSContext>(), "Arbitrum One", configuration.GetSection("APIKeys").GetValue<string>("Arbiscan"));
+            arbiscanLogger.LogDataAsync();
             while (true) { await Task.Delay(1); }
         }
 
-        private static async void UpdateMainnetTPS(ETHTPSContext context, string apiKey)
-        {
-            while (true)
-            {
-                var client = new HttpClient();
-                try
-                {
-                    int latestBlock = HexToDec(JsonConvert.DeserializeObject<dynamic>(await client.GetStringAsync($"https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey={apiKey}")).result.ToString());
-                    int blockTransactions = HexToDec(JsonConvert.DeserializeObject<dynamic>(await client.GetStringAsync($"https://api.etherscan.io/api?module=proxy&action=eth_getBlockTransactionCountByNumber&apikey={apiKey}&tag={latestBlock.ToString("X")}")).result.ToString());
-
-                    var provider = context.Providers.First(x => x.Name == "Ethereum");
-                    var data = new TPSData()
-                    {
-                        Block = latestBlock.ToString(),
-                        Date = DateTime.Now,
-                        Provider = provider.Id,
-                        Tps = (int)(Math.Ceiling(blockTransactions / 13.1))
-                    };
-                    if (!context.Tpsdata.Any(x => x.Provider.Value == provider.Id && x.Block == latestBlock.ToString()))
-                    {
-                        context.Tpsdata.Add(data);
-                    }
-                    context.SaveChanges();
-                    Console.WriteLine($"Block {latestBlock} transaction count: {blockTransactions} ({data.Tps} tps)");
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                }
-                await Task.Delay(10000);
-            }
-        }
-
-        private static DateTime LatestBlockTimeUpdate = DateTime.MinValue;
-        private static double LatestBlockTime = 0;
-        private static async Task<double> GetEthereumLatestBlockTime()
-        {
-            if (DateTime.Now.Subtract(LatestBlockTimeUpdate).TotalMinutes > 1)
-            {
-                LatestBlockTimeUpdate = DateTime.Now;
-                var client = new HttpClient();
-                try
-                {
-                    var latest = JsonConvert.DeserializeObject<dynamic>(await client.GetStringAsync("https://api.blockcypher.com/v1/eth/main")).latest_url;
-                    var data = JsonConvert.DeserializeObject<dynamic>(await client.GetStringAsync(latest.ToString()));
-                    var t1 = (DateTime)data.time;
-                    var data2 = JsonConvert.DeserializeObject<dynamic>(await client.GetStringAsync(data.prev_block_url.ToString()));
-                    var t2 = (DateTime)data2.time;
-                    LatestBlockTime = t1.Subtract(t2).TotalSeconds;
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                }
-            }
-            return LatestBlockTime;
-        }
-
-        private static int HexToDec(string hex)
-        {
-            return int.Parse(hex.Replace("0x", string.Empty), System.Globalization.NumberStyles.HexNumber);
-        }
+        public static object LockObject = new object();
     }
 }
