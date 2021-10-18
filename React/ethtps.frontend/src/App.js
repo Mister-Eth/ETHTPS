f5756669d9d145fdcb91faebcb62107ab7b05242
WhoEvenAmI <automationbot9000@gmail.com>
2021-10-18 14:33:00 +0300

    Add top bar

---
+++ React/ethtps.frontend/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import TopBar from './components/TopBar'
+
+function App() {
+  return (
+    <TopBar></TopBar>
+  );
+}
+
+export default App;
---
+++ React/ethtps.frontend/src/components/TopBar.js
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+
+export default function TopBar() {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="small"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            ETHTPS.info
+          </Typography>
+          <Button color="inherit">Login</Button>
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
