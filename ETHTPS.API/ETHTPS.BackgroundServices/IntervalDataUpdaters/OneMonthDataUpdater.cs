850f514f729d1c2865f5a4bfebc2438689300381
Mister_Eth <89896648+WhoEvenAmI@users.noreply.github.com>
2021-10-22 18:35:00 +0300

    Update OneMonthDataUpdater.cs

--- ETHTPS.API/ETHTPS.BackgroundServices/IntervalDataUpdaters/OneMonthDataUpdater.cs
+++ ETHTPS.API/ETHTPS.BackgroundServices/IntervalDataUpdaters/OneMonthDataUpdater.cs
@@ -15,7 +15,7 @@ namespace ETHTPS.BackgroundServices.IntervalDataUpdaters
 {
     public class OneMonthDataUpdater : IntervalDataUpdaterBase
     {
-        public OneMonthDataUpdater(ILogger<BackgroundServiceBase> logger, IServiceScopeFactory serviceScopeFactory) : base(logger, serviceScopeFactory, "OneMonth", TimeSpan.FromHours(1))
+        public OneMonthDataUpdater(ILogger<BackgroundServiceBase> logger, IServiceScopeFactory serviceScopeFactory) : base(logger, serviceScopeFactory, "OneMonth", TimeSpan.FromHours(24))
         {
         }
 
