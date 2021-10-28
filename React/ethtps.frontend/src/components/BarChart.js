5307e574e6ee892ac34d81f81f319fae425e3511
Mister_Eth <89896648+WhoEvenAmI@users.noreply.github.com>
2021-10-28 15:56:08 +0300

    Update BarChart.js

--- React/ethtps.frontend/src/components/BarChart.js
+++ React/ethtps.frontend/src/components/BarChart.js
@@ -1,13 +1,42 @@
 import { Component } from 'react';
+import { Bar } from 'react-chartjs-2';
 
 class BarChart extends Component{
     constructor(props){
         super(props);
     }
 
+     labels = ["J", "F", "M", "A", "J", "J", "A"]
+ data = {
+  labels: this.labels,
+  datasets: [{
+    label: 'My First Dataset',
+    data: [65, 59, 80, 81, 56, 55, 40],
+    backgroundColor: [
+      'rgba(255, 99, 132, 0.2)',
+      'rgba(255, 159, 64, 0.2)',
+      'rgba(255, 205, 86, 0.2)',
+      'rgba(75, 192, 192, 0.2)',
+      'rgba(54, 162, 235, 0.2)',
+      'rgba(153, 102, 255, 0.2)',
+      'rgba(201, 203, 207, 0.2)'
+    ],
+    borderColor: [
+      'rgb(255, 99, 132)',
+      'rgb(255, 159, 64)',
+      'rgb(255, 205, 86)',
+      'rgb(75, 192, 192)',
+      'rgb(54, 162, 235)',
+      'rgb(153, 102, 255)',
+      'rgb(201, 203, 207)'
+    ],
+    borderWidth: 1
+  }]
+};
+
     render(){
         return <>
-        
+            <Bar data={this.data}/>
         </>;
     }
 }
