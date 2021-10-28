e0e3696a4b5db75f16bb6589036f043e58e355dd
Mister_Eth <89896648+WhoEvenAmI@users.noreply.github.com>
2021-10-28 12:23:53 +0300

    Fix footer

--- React/ethtps.frontend/src/App.js
+++ React/ethtps.frontend/src/App.js
@@ -4,8 +4,8 @@ import githubIcon from './assets/600px-Octicons-mark-github.svg - inv.png';
 import twitterIcon from './assets/1486053611-twitter_79195.png';
 import discordIcon from './assets/discord-mascot.png';
 import logo200 from './assets/logo200.png'
-import { useEffect } from 'react';
 import { globalApi } from './services/common';
+import React, { useState, useEffect } from "react";
 
 function App() {
 
@@ -15,18 +15,41 @@ function App() {
   useEffect(async() => {
     globalApi.aPIV2HomePageModelGet(network, (err,data,res) => console.log(data))
   });
-
   return (
     <>
+    <center>
+     <a href="/">
+      <p className={'site-logo'}>
+          ethtps.info
+      </p>
+      </a>
+      <a href="https://github.com/WhoEvenAmI/ETHTPS">
+          <img className={"small-img"} src={githubIcon}>
+          </img>
+        </a>
+        <a href="https://twitter.com/ethtps">
+          <img className={"small-img"} src={twitterIcon}>
+          </img>
+        </a>
+        <a href="https://discord.gg/jWPcsTzpCT">
+          <img className={"small-img"} src={discordIcon}>
+          </img>
+        </a>
+    </center>
     <footer>
-      <p className={'inline'}>
-      Brought to you by Mister_Eth
+      <div className={'inline'}>
+      Brought to you by 
+      <div  style={{marginLeft:'5px'}} className={"tooltip"}>
+        <span>
+          Mister_Eth
+        </span>
+      </div>
       <br></br>
         Donate: 
         <p className={'ul inline'} style={{marginLeft:'5px'}}>
           0x466Ef24d68ac9b61670eeE7fC2E001B951aBf482
         </p>
-      </p>
+      </div>
     </footer>
     </>
   );
