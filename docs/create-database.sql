687ef8fe5d85102ce368aa36f89125befbcb9e08
Mister_Eth <89896648+WhoEvenAmI@users.noreply.github.com>
2021-11-03 09:33:28 +0200

    Update db

--- docs/create-database.sql
+++ docs/create-database.sql
@@ -1,173 +1,150 @@
 CREATE TABLE [Providers] (
   [ID] int PRIMARY KEY IDENTITY(1, 1),
-  [Name] nvarchar(255),
-  [Type] int
+  [Name] nvarchar(255) UNIQUE NOT NULL,
+  [Type] int NOT NULL
 )
 GO
 
 CREATE TABLE [ProviderTypes] (
   [ID] int PRIMARY KEY IDENTITY(1, 1),
-  [Name] nvarchar(255)
+  [Name] nvarchar(255) UNIQUE NOT NULL
+)
+GO
+
+CREATE TABLE [ProviderTypeProperties] (
+  [ID] int PRIMARY KEY IDENTITY(1, 1),
+  [ProviderType] int NOT NULL,
+  [Name] nvarchar(255) NOT NULL,
+  [Value] nvarchar(255) NOT NULL
 )
 GO
 
 CREATE TABLE [ProviderProperties] (
   [ID] int PRIMARY KEY IDENTITY(1, 1),
-  [Provider] int,
-  [Name] nvarchar(255),
-  [Value] nvarchar(255)
-)
-GO
-
-CREATE TABLE [TPSData] (
-  [ID] int PRIMARY KEY IDENTITY(1, 1),
-  [Network] int,
-  [Provider] int,
-  [Date] datetime,
-  [Block] nvarchar(255),
-  [TPS] float
+  [Provider] int NOT NULL,
+  [Name] nvarchar(255) NOT NULL,
+  [Value] nvarchar(255) NOT NULL
 )
 GO
 
 CREATE TABLE [AccesStats] (
   [ID] int PRIMARY KEY IDENTITY(1, 1),
-  [Project] nvarchar(255),
-  [Path] nvarchar(255),
-  [Count] int
+  [Project] nvarchar(255) NOT NULL,
+  [Path] nvarchar(255) NOT NULL,
+  [Count] int NOT NULL,
+  [AverageRequestTimeMs] float NOT NULL,
+  [ExternalCount] int NOT NULL
 )
 GO
 
 CREATE TABLE [CachedResponses] (
   [ID] int PRIMARY KEY IDENTITY(1, 1),
-  [Name] nvarchar(255),
+  [Name] nvarchar(255) UNIQUE NOT NULL,
   [JSON] varchar(max)
 )
 GO
 
 CREATE TABLE [Networks] (
   [ID] int PRIMARY KEY IDENTITY(1, 1),
-  [Name] nvarchar(255)
+  [Name] nvarchar(255) UNIQUE NOT NULL
 )
 GO
 
-CREATE TABLE [LatestEntries] (
+CREATE TABLE [TPSAndGasData_Latest] (
   [ID] int PRIMARY KEY IDENTITY(1, 1),
-  [Provider] int,
-  [Entry] int
+  [Provider] int NOT NULL,
+  [TPS] float NOT NULL,
+  [GPS] float NOT NULL
 )
 GO
 
-CREATE TABLE [TaskPerformanceMetrics] (
+CREATE TABLE [TPSAndGasData_Max] (
   [ID] int PRIMARY KEY IDENTITY(1, 1),
-  [Machine] int,
-  [TaskName] nvarchar(255),
-  [RunCount] int,
-  [AverageRunTime] float
+  [Provider] int NOT NULL,
+  [MaxTPS] float NOT NULL,
+  [MaxGPS] float NOT NULL
 )
 GO
 
-CREATE TABLE [MachineConfigurations] (
+CREATE TABLE [TPSAndGasData_Hour] (
   [ID] int PRIMARY KEY IDENTITY(1, 1),
-  [Name] nvarchar(255),
-  [CPUCoreCount] int,
-  [TotalRAM] int
+  [Network] int NOT NULL,
+  [Provider] int NOT NULL,
+  [StartDate] datetime NOT NULL,
+  [AverageTPS] float NOT NULL,
+  [AverageGPS] float NOT NULL,
+  [ReadingsCount] int NOT NULL
 )
 GO
 
-CREATE TABLE [MaxTPSEntries] (
+CREATE TABLE [TPSAndGasData_Day] (
   [ID] int PRIMARY KEY IDENTITY(1, 1),
-  [Provider] int,
-  [Entry] int
+  [Network] int NOT NULL,
+  [Provider] int NOT NULL,
+  [StartDate] datetime NOT NULL,
+  [AverageTPS] float NOT NULL,
+  [AverageGPS] float NOT NULL,
+  [ReadingsCount] int NOT NULL
 )
 GO
 
-CREATE TABLE [TPSData_Hour] (
+CREATE TABLE [TPSAndGasData_Week] (
   [ID] int PRIMARY KEY IDENTITY(1, 1),
-  [Network] int,
-  [Provider] int,
-  [StartDate] datetime,
-  [AverageTPS] float,
-  [ReadingsCount] int
+  [Network] int NOT NULL,
+  [Provider] int NOT NULL,
+  [StartDate] datetime NOT NULL,
+  [AverageTPS] float NOT NULL,
+  [AverageGPS] float NOT NULL,
+  [ReadingsCount] int NOT NULL
 )
 GO
 
-ALTER TABLE [TPSData_Hour] ADD FOREIGN KEY ([Provider]) REFERENCES [Providers] ([ID])
-GO
-
-ALTER TABLE [TPSData_Hour] ADD FOREIGN KEY ([Network]) REFERENCES [Networks] ([ID])
-GO
-
-CREATE TABLE [TPSData_Day] (
+CREATE TABLE [TPSAndGasData_Month] (
   [ID] int PRIMARY KEY IDENTITY(1, 1),
-  [Network] int,
-  [Provider] int,
-  [StartDate] datetime,
-  [AverageTPS] float,
-  [ReadingsCount] int
+  [Network] int NOT NULL,
+  [Provider] int NOT NULL,
+  [StartDate] datetime NOT NULL,
+  [AverageTPS] float NOT NULL,
+  [AverageGPS] float NOT NULL,
+  [ReadingsCount] int NOT NULL
 )
-GO
-
-ALTER TABLE [TPSData_Day] ADD FOREIGN KEY ([Provider]) REFERENCES [Providers] ([ID])
-GO
-
-ALTER TABLE [TPSData_Day] ADD FOREIGN KEY ([Network]) REFERENCES [Networks] ([ID])
-GO
-
-CREATE TABLE [TPSData_Week] (
-  [ID] int PRIMARY KEY IDENTITY(1, 1),
-  [Network] int,
-  [Provider] int,
-  [StartDate] datetime,
-  [AverageTPS] float,
-  [ReadingsCount] int
-)
-GO
-
-ALTER TABLE [TPSData_Week] ADD FOREIGN KEY ([Provider]) REFERENCES [Providers] ([ID])
-GO
-
-ALTER TABLE [TPSData_Week] ADD FOREIGN KEY ([Network]) REFERENCES [Networks] ([ID])
-GO
-
-CREATE TABLE [TPSData_Month] (
-  [ID] int PRIMARY KEY IDENTITY(1, 1),
-  [Network] int,
-  [Provider] int,
-  [StartDate] datetime,
-  [AverageTPS] float,
-  [ReadingsCount] int
-)
-GO
-
-ALTER TABLE [TPSData_Month] ADD FOREIGN KEY ([Provider]) REFERENCES [Providers] ([ID])
-GO
-
-ALTER TABLE [TPSData_Month] ADD FOREIGN KEY ([Network]) REFERENCES [Networks] ([ID])
-GO
-
-ALTER TABLE [MaxTPSEntries] ADD FOREIGN KEY ([Provider]) REFERENCES [Providers] ([ID])
-GO
-
-ALTER TABLE [MaxTPSEntries] ADD FOREIGN KEY ([Entry]) REFERENCES [TPSData] ([ID])
 GO
 
 ALTER TABLE [Providers] ADD FOREIGN KEY ([Type]) REFERENCES [ProviderTypes] ([ID])
 GO
 
+ALTER TABLE [ProviderTypeProperties] ADD FOREIGN KEY ([ProviderType]) REFERENCES [ProviderTypes] ([ID])
+GO
+
 ALTER TABLE [ProviderProperties] ADD FOREIGN KEY ([Provider]) REFERENCES [Providers] ([ID])
 GO
 
-ALTER TABLE [TPSData] ADD FOREIGN KEY ([Provider]) REFERENCES [Providers] ([ID])
+ALTER TABLE [TPSAndGasData_Latest] ADD FOREIGN KEY ([Provider]) REFERENCES [TPSAndGasData_Latest] ([ID])
 GO
 
-ALTER TABLE [TPSData] ADD FOREIGN KEY ([Network]) REFERENCES [Networks] ([ID])
+ALTER TABLE [TPSAndGasData_Max] ADD FOREIGN KEY ([Provider]) REFERENCES [TPSAndGasData_Latest] ([ID])
 GO
 
-ALTER TABLE [TaskPerformanceMetrics] ADD FOREIGN KEY ([Machine]) REFERENCES [MachineConfigurations] ([ID])
+ALTER TABLE [TPSAndGasData_Hour] ADD FOREIGN KEY ([Provider]) REFERENCES [Providers] ([ID])
 GO
 
-ALTER TABLE [LatestEntries] ADD FOREIGN KEY ([Provider]) REFERENCES [Providers] ([ID])
+ALTER TABLE [TPSAndGasData_Hour] ADD FOREIGN KEY ([Network]) REFERENCES [Networks] ([ID])
 GO
 
-ALTER TABLE [LatestEntries] ADD FOREIGN KEY ([Entry]) REFERENCES [TPSData] ([ID])
+ALTER TABLE [TPSAndGasData_Day] ADD FOREIGN KEY ([Provider]) REFERENCES [Providers] ([ID])
+GO
+
+ALTER TABLE [TPSAndGasData_Day] ADD FOREIGN KEY ([Network]) REFERENCES [Networks] ([ID])
+GO
+
+ALTER TABLE [TPSAndGasData_Week] ADD FOREIGN KEY ([Provider]) REFERENCES [Providers] ([ID])
+GO
+
+ALTER TABLE [TPSAndGasData_Week] ADD FOREIGN KEY ([Network]) REFERENCES [Networks] ([ID])
+GO
+
+ALTER TABLE [TPSAndGasData_Month] ADD FOREIGN KEY ([Provider]) REFERENCES [Providers] ([ID])
+GO
+
+ALTER TABLE [TPSAndGasData_Month] ADD FOREIGN KEY ([Network]) REFERENCES [Networks] ([ID])
 GO
