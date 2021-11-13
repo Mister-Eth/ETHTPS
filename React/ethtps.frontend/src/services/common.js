9bb1cd3120a9557f14648de6decc8ce6bcbbd6ad
Mister_Eth <89896648+WhoEvenAmI@users.noreply.github.com>
2021-11-13 12:57:31 +0200

    Update common.js

--- React/ethtps.frontend/src/services/common.js
+++ React/ethtps.frontend/src/services/common.js
@@ -1,8 +1,9 @@
 import { GeneralApi, GPSApi, TPSApi } from './api-gen/src/index';
 import ApiClient from './api-gen/src/ApiClient';
+import InstantDataService from './InstantDataService';
 
-const client = new ApiClient('https://api.ethtps.info');
 const globalGeneralApi = new GeneralApi(client);
 const globalGPSApi = new GPSApi(client);
 const globalTPSApi = new TPSApi(client);
-export { globalGeneralApi, globalGPSApi, globalTPSApi };
+const globalInstantDataService = new InstantDataService(); 
+export { globalGeneralApi, globalGPSApi, globalTPSApi, globalInstantDataService };
