965c8fae73ad76123ef42abe35fb44b1985ec093
Mister_Eth <89896648+WhoEvenAmI@users.noreply.github.com>
2021-11-16 09:31:34 +0200

    Update ArbitrumDetails.js

--- React/ethtps.frontend/src/components/pages/networks/details/ArbitrumDetails.js
+++ React/ethtps.frontend/src/components/pages/networks/details/ArbitrumDetails.js
@@ -1,20 +1,45 @@
 import React from "react";
+import NetworkSummary from "../NetworkSummary";
 
 export default class ArbitrumDetails extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            description: "No data is available right now",
-            upgradePlans: ""
+            details: {
+                description:
+                  'Arbitrum is an Optimistic Rollup that aims to feel exactly like interacting with Ethereum, but with transactions costing a fraction of what they do on L1.',
+                purpose: 'Universal',
+                links: {
+                  websites: ['https://arbitrum.io/', 'https://offchainlabs.com/'],
+                  documentation: ['https://developer.offchainlabs.com/'],
+                  explorers: ['https://arbiscan.io', 'https://explorer.arbitrum.io/'],
+                  repositories: [
+                    'https://github.com/OffchainLabs/arbitrum',
+                    'https://github.com/OffchainLabs/arb-os',
+                  ],
+                  socialMedia: [
+                    'https://twitter.com/OffchainLabs',
+                    'https://twitter.com/arbitrum',
+                    'https://medium.com/offchainlabs',
+                    'https://discord.gg/5KE54JwyTs',
+                  ]
+                }
+            },
+            name: props.name
         }
     }
     
     render(){
         return <>
-            <h1>Description</h1>
-            {this.state.description}
-            <h1>Upgrade plans</h1>
-            {this.state.upgradePlans}
-        </>
+           <div className={'fcontainer'}>
+                <div className={'fitem'} style={{float:'right'}}>
+                    <NetworkSummary name={this.state.name} details={this.state.details.links}/>
+                </div>
+                <div className={'fitem s-padding'}>
+                    <h1>Description</h1>
+                    {this.state.details.description}
+                </div>
+           </div>
+        </>;
     }
 }
