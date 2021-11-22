532d7cc0554fecebc0c4a71eed3e6dac4f931cf6
Mister_Eth <89896648+Mister-Eth@users.noreply.github.com>
2021-11-22 11:36:31 +0200

    Add Arbitrum and Optimism details

--- React/ethtps.frontend/src/components/pages/networks/details/ArbitrumDetails.js
+++ React/ethtps.frontend/src/components/pages/networks/details/ArbitrumDetails.js
@@ -38,6 +38,31 @@ export default class ArbitrumDetails extends React.Component {
                 <div className={'fitem s-padding'}>
                     <h1>Description</h1>
                     {this.state.details.description}
+                    
+                <h1>Technology</h1>
+                <p>
+                To date, two main styles of L2 rollups have come to the fore: zero-knowledge (ZK) rollups and optimistic rollups. 
+<br/><br/>
+All rollups handle transaction execution off-chain, meaning they use their own infrastructure to facilitate transactions. In this way, rollups are like decongestants for Ethereum. They relieve Ethereum by readily supporting Ethereum-based activities that would have otherwise competed for “L1” Ethereum block space. Stylistically speaking, what sets rollups apart derives from how they handle data. 
+<br/><br/>
+Rollups execute transactions off-chain, but in some capacity these L2s post data to the Ethereum mainnet. How they do determines whether they’re a ZK rollup or an optimistic rollup. The former rely on validity proofs, which entail posting batches of transactions via cryptographic proofs called ZK-SNARKs. The latter rely on fraud proofs, which assume posted data is valid unless challenged. 
+<br/><br/>       
+Because Arbitrum’s technology depends on fraud proofs, it is “optimistic” in style rather than ZK-based. Zooming in, this style affords Arbitrum some fundamental advantages:
+<br/><br/></p>
+<ul>
+  <li>
+  Optimistic rollups are readily compatible with the Ethereum Virtual Machine (EVM), so L1 dapps can easily “port” their projects to Arbitrum’s L2 tech. 
+
+  </li>
+  <li>
+  Optimistic rollups also offer low fixed-gas costs per transaction batch and lower off-chain computation costs than ZK rollups face, so Arbitrum enjoys these structural benefits. 
+
+  </li>
+</ul>
+<p>
+Of course, it’s worth repeating that along with these specialized advantages Arbitrum additionally enjoys the generalized advantages of rollups. These are rapid and inexpensive transactions and the decentralization and security guarantees provided by Ethereum as a reliable L1 foundation. 
+                </p>
+                <a href={'https://defipulse.com/blog/what-is-arbitrum/'}>Source</a>
                 </div>
            </div>
         </>;
--- React/ethtps.frontend/src/components/pages/networks/details/OptimismDetails.js
+++ React/ethtps.frontend/src/components/pages/networks/details/OptimismDetails.js
@@ -10,7 +10,7 @@ export default class OptimismDetails extends React.Component {
                 warning:
                   'Currently only whitelisted contracts can be deployed on Optimism.',
                 description:
-                  'Optimistic Ethereum is an EVM-compatible Optimistic Rollup chain. It aims to be fast, simple, and secure.',
+                  'Optimism is a Layer 2 scaling solution for Ethereum that can support all of Ethereum’s Dapps. Instead of running all computation and data on the Ethereum network, Optimism puts all transaction data on-chain and runs computation off-chain, increasing Ethereum’s transactions per second and decreasing transaction fees. Tests have shown a 143x decrease in transaction fees on Synthetix Exchange and up to a 100x decrease on Uniswap. Since transaction data still stays on the Ethereum network, this scaling solution does not sacrifice Ethereum’s decentralisation or security for scalability.',
                 purpose: 'Universal',
                 links: {
                   websites: ['https://optimism.io/'],
@@ -38,6 +38,34 @@ export default class OptimismDetails extends React.Component {
                 <div className={'fitem s-padding'}>
                     <h1>Description</h1>
                     {this.state.details.description}
+                    <h1>Technology</h1>
+                    <p>
+                    Sequencers on Optimism are responsible for executing computation off-chain and publishing compressed transaction data onto a smart contract on Ethereum at regular checkpoints. Since computation is resource-intensive, moving it off Ethereum allows it to scale by orders of magnitude.
+<br/><br/>
+But what if sequencers are malicious and send fraudulent data? When the sequencer publishes transaction data, there is a window of time where anyone can run their own computation to determine if the transaction data is fraudulent. If the data was indeed fraudulent, the verifier runs the computation on-chain and the smart contract will verify the data. The sequencer will then lose their deposit, a part of which will be sent as a reward to the verifier, and the other amount burnt. As all transaction data is submitted on-chain, a new sequencer will be able to compute the data and replace the role of the previous sequencer. 
+<br/><br/>
+This creates:
+                    </p>
+                    <ul>
+                      <li>
+                      An economic incentive for sequencers to act honestly 
+                      </li>
+                      <li>
+                      An economic incentive for verifiers to check on the sequencers
+                      </li>
+                    </ul>
+                    <p>
+                    There will also be other parties that are incentivised to verify sequencer data, such as stakeholders on Optimism or stakeholders of Dapps deployed on Optimism. 
+However, there are still two issues with Optimism:
+<br/><br/>
+    1. Seven day lock-up periods for withdrawing back onto Ethereum 
+    <br/><br/>
+Since there is a window of time where verifiers run fraud proofs to check on sequencers, users have a waiting period before withdrawing their funds back to Ethereum. This issue is easily solved with liquidity providers that help users instantly withdraw funds back to Ethereum for a fee. 
+<br/><br/>
+    2. Composability 
+    <br/><br/>
+Dapps on Optimism cannot interact with other Dapps deployed on Ethereum or other Layer 2 scaling solutions in the same transaction. But since every application on Ethereum can be copied onto Optimism easily, there may be not much need for cross-chain composability. That said, Connext Network will be tackling this issue.  
+                    </p>
                 </div>
            </div>
         </>
