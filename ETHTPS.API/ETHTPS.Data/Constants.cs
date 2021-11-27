381ef58be7ff2c41aaad2d27d0f304bb7ad07d64
Mister_Eth <89896648+Mister-Eth@users.noreply.github.com>
2021-11-27 18:25:31 +0200

    Create Constants.cs

---
+++ ETHTPS.API/ETHTPS.Data/Constants.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ETHTPS.Data
+{
+    public static class Constants
+    {
+        public static class CacheTimes
+        {
+            public static TimeSpan Realtime = TimeSpan.FromSeconds(3);
+            public static TimeSpan LowUpdateRate = TimeSpan.FromMinutes(1);
+        }
+    }
+}
