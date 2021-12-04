1e129f26d3c966847d40a0c167227914e8b14db6
Mister_Eth <89896648+Mister-Eth@users.noreply.github.com>
2021-12-04 15:03:57 +0200

    Update InstantDataStat.js

--- React/ethtps.frontend/src/components/pages/MainPage/components/instant-stats/bar/InstantDataStat.js
+++ React/ethtps.frontend/src/components/pages/MainPage/components/instant-stats/bar/InstantDataStat.js
@@ -55,7 +55,7 @@ class InstantDataStat extends Component{
         if (state.data === undefined || state.data.length === 0)
             return 20;
         
-        let t = state.providerData.filter(x=>state.data[x.name] !== undefined).map(x=>state.data[x.name][0].value);
+        let t = state.providerData.filter(x=>state.data[x.name] !== undefined  && state.data[x.name][0] !== null).map(x=>state.data[x.name][0].value);
         if (t.length === 0){
             return 0;
         }
