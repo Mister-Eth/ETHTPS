59e4b7dfd92720b9124638607948fbcf7d9e51c3
Mister_Eth <89896648+Mister-Eth@users.noreply.github.com>
2022-06-01 16:18:41 +0300

    Update common.js

--- React/ethtps.frontend/src/services/common.js
+++ React/ethtps.frontend/src/services/common.js
@@ -8,40 +8,44 @@ export const globalGPSApi = new GPSApi(client);
 export const globalTPSApi = new TPSApi(client);
 export const globalGasAdjustedTPSApi = new GasAdjustedTPSApi(client);
 export const globalStatusApi = new StatusApi(client);
-export const globalInstantDataService = new InstantDataService(); 
+export const globalInstantDataService = new InstantDataService();
 export const globalTimeWarpApi = new TimeWarpApi(client);
-export const formatModeName = function(mode) {
-    if (mode !== "gasAdjustedTPS"){
-      return mode.toUpperCase();
-    }
-    else{
-      return "gas-adjusted TPS"
-    }
+export const formatModeName = function (mode) {
+  if (mode !== "gasAdjustedTPS") {
+    return mode.toUpperCase();
   }
-
-export const isEmpty = function(obj) {
-    for(var prop in obj) {
-        if(obj.hasOwnProperty(prop))
-            return false;
-    }
-
-    return true;
+  else {
+    return "gas-adjusted TPS"
+  }
 }
 
-export const formatSmoothingName = function(smoothing){
-    smoothing = smoothing.replace("One", "1")
+export const isEmpty = function (obj) {
+  for (var prop in obj) {
+    if (obj.hasOwnProperty(prop))
+      return false;
+  }
+
+  return true;
+}
+
+export const formatSmoothingName = function (smoothing) {
+  smoothing = smoothing.replace("One", "1")
     .replace("Minute", "m")
     .replace("Hour", "h")
     .replace("Day", "d")
     .replace("Week", "w")
     .replace("Month", "mo")
-    return smoothing;
+  return smoothing;
 }
 
-export const capitalizeFirstLetter = function(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
+export const capitalizeFirstLetter = function (string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 
-export const to2DecimalPlaces = function(num){
-    return Math.round((num + Number.EPSILON) * 100) / 100
- }
+export const to2DecimalPlaces = function (num) {
+  return Math.round((num + Number.EPSILON) * 100) / 100
+}
+
+export const addThousandsSeparators = function (number) {
+  return parseFloat(number).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
