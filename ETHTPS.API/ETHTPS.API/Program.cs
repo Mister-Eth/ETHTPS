7b17f6a906bfc97e3348fc4ba8126e8d29684f80
Mister ETH <89896648+Mister-Eth@users.noreply.github.com>
2023-01-02 14:53:27 +0200

    Update Program.cs

--- ETHTPS.API/ETHTPS.API/Program.cs
+++ ETHTPS.API/ETHTPS.API/Program.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace ETHTPS.API
