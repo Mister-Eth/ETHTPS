590cc41ffe9725eeee21768129da97af315f5cdc
Mister_Eth <89896648+Mister-Eth@users.noreply.github.com>
2023-01-27 05:23:27 +0200

    Update DataValueCell.tsx

--- React/ethtps.frontend.v2/src/components/tables/all networks/cells/DataValueCell.tsx
+++ React/ethtps.frontend.v2/src/components/tables/all networks/cells/DataValueCell.tsx
@@ -2,32 +2,33 @@ import { ICustomCellConfiguration } from "./ICustomCellConfiguration"
 import { SkeletonWithTooltip } from "../../../partials/SkeletonWithTooltip"
 import { TableCell, Typography } from "@mui/material"
 import { useGetLiveDataFromAppStore } from "../../../../hooks/LiveDataHooks"
-import {
-  useGetLiveDataModeFromAppStore,
-  useGetLiveDataSmoothingFromAppStore,
-} from "../../../../hooks/LiveDataHooks"
+import { useGetLiveDataModeFromAppStore } from "../../../../hooks/LiveDataHooks"
 import { toShortString } from "../../../../Types"
-import { ILiveDataModeModel } from "../../../../models/interfaces/ILiveDataModeModel"
 import { tableCellTypographyStandard } from "./Typography.types"
 import { centered } from "../../Cells.Types"
+import { useEffect, useState } from "react"
 
 export function DataValueCell(config: ICustomCellConfiguration) {
   const mode = useGetLiveDataModeFromAppStore()
   const liveData = useGetLiveDataFromAppStore()
-  let value: number = 0
-  if (liveData.data !== undefined) {
-    value = liveData.data[config.provider?.name as string]?.at(0)?.data?.at(0)
-      ?.value as number
-  }
-  value = value === undefined ? 0 : Math.trunc(value * 100) / 100
+  const [value, setValue] = useState<number | undefined>(undefined)
+  useEffect(() => {
+    let x: number = 0
+    if (liveData.data !== undefined) {
+      x = liveData.data[config.provider?.name as string]?.at(0)?.data?.at(0)
+        ?.value as number
+    }
+    x = x === undefined ? 0 : Math.trunc(x * 100) / 100
+    setValue(x)
+  }, [mode, liveData])
   return (
-    <TableCell>
+    <TableCell {...centered}>
       {value === undefined ? (
         <SkeletonWithTooltip
           text={`Loading ${config.provider?.name} ${toShortString(mode)}...`}
         />
       ) : (
-        <Typography {...tableCellTypographyStandard} {...centered}>
+        <Typography {...tableCellTypographyStandard}>
           {value as number}
         </Typography>
       )}
